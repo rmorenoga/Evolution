e7f82e9ed18f3e36df46737c9be80e8a69084740
rodr <rodr@ubuser-DskTop>
2016-05-10 19:03:25 -0500

    Started a proper simulation class, and robot class, created hormone enabled module models that comply with V-REP's updated simulation strategy

---
+++ VREPControl/src/Tests/HormControlTest.java
@@ -0,0 +1,63 @@
+package Tests;
+
+public class HormControlTest {
+	
+	public static void main(String[] args) throws InterruptedException{
+		// Use simulator instance 0 or the default remoteAPI server
+				// configuration
+				int simulator= 0;
+				
+				// Morphology Parameters
+						int Numberofmodules = 8;
+						int[] orientation = new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
+						
+						// Simulation Parameters
+						int MaxTime = 10;
+						
+						float[] ampset = new float []{0.5f,0.4f,0.3f,0.5f,0.6f,0.3f,0.5f};
+						float[] offsetset = new float[] {0.1f,-0.1f,0.2f,0.3f,-0.4f,0.3f,0.4f} ;
+						float[] phaseset = new float[] {0.3f,0.2f,0.35f,0.4f,0.3f,0.2f,0.3f};
+						float[] ampstep = new float []{0.01f,0.01f,0.01f,0.01f,0.01f,0.01f,0.01f};
+						float[] offstep = new float []{0.01f,0.01f,0.01f,0.01f,0.01f,0.01f,0.01f};
+						float[] phasestep = new float []{0.01f,0.01f,0.01f,0.01f,0.01f,0.01f,0.01f};
+						
+						float[] CP = new float[]{42};
+						for (int i = 0; i < 7; i++) {
+							CP[i] = ampset[i];
+							CP[i+7] = offsetset[i];
+							CP[i+14] = phaseset[i];
+							CP[i+21] = ampstep[i];
+							CP[i+28] = offstep[i];
+							CP[i+35] = phasestep[i];
+						}
+						
+						// Array that receives fitness from the simulator or signals a crash
+						float[] rfitness = new float[3];
+						
+						RobotTest robot = new RobotTest(Numberofmodules,orientation,CP);
+						
+						SimulationTest sim = new SimulationTest(simulator,MaxTime,robot);
+						
+						if(sim.Connect()){
+							
+							sim.SendSignals();
+							
+							rfitness = sim.RunSimulation(0.7f);
+							
+							sim.Disconnect();
+							
+							
+							
+							
+						}else{
+							// No connection could be established
+							System.out.println("Failed connecting to remote API server");
+						}
+						
+						System.out.println("Fitness = " + rfitness[1]);
+						
+						
+						
+	}
+
+}
---
+++ VREPControl/src/Tests/RobotTest.java
@@ -0,0 +1,68 @@
+package Tests;
+
+public class RobotTest {
+
+	// Morphology Parameters
+	/**
+	 * Number of modules in the current configuration
+	 */
+	int Numberofmodules = 8;
+	
+	/**
+	 * Orientation of the modules in the configuration
+	 */
+	int[] orientation = new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
+	
+	/**
+	 * The control parameters of the robot
+	 */
+	float[] ControlParam;
+	
+	
+	/**
+	 * Creates a robot configuration object
+	 * @param Numberofmodules the number of modules in the current configuration 
+	 * @param orientation the orientation of the modules in the configuration
+	 * @param ControlParam the control parameters of the robot
+	 */
+	public RobotTest(int Numberofmodules, int[] orientation, float[] ControlParam){
+		this.Numberofmodules = Numberofmodules;
+		this.orientation = orientation;
+		this.ControlParam = ControlParam;
+	}
+
+
+	public int getNumberofmodules() {
+		return Numberofmodules;
+	}
+
+
+	public void setNumberofmodules(int numberofmodules) {
+		Numberofmodules = numberofmodules;
+	}
+
+
+	public int[] getOrientation() {
+		return orientation;
+	}
+
+
+	public void setOrientation(int[] orientation) {
+		this.orientation = orientation;
+	}
+
+
+	public float[] getControlParam() {
+		return ControlParam;
+	}
+
+
+	public void setControlParam(float[] controlParam) {
+		ControlParam = controlParam;
+	}
+	
+	
+	
+	
+	
+}
