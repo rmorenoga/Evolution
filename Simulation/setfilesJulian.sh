ce96e7b1a02830548662c8a4aa1f0c6d95850757
rodr <rodr@rodr-VirtualBox>
2016-08-12 18:09:19 -0500

    Could not implement post-merge hook on remote server.
    Switched to a script instead.

---
+++ Simulation/setfilesJulian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copies the simulation files in the corresponding simulators folders
+#
+
+
+
+
+
+
+cp -r ./Maze /home/julian/V-REP/Vrep0/scenes/
+cp -r ./corridors /home/julian/V-REP/Vrep0/models/
+cp -r ./Module /home/julian/V-REP/Vrep0/models/ 
