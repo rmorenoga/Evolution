2ff87bbe1edb8021874f30d05772298302e1cb34
rodr <rodr@rodr-VirtualBox>
2016-08-12 16:12:46 -0500

    Ditched pre-commit, using bash script instead.
    Commiting the real simulation files after pre-commit script fail.

--- Simulation/getfiles.sh
+++ Simulation/getfiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cp -r /home/rodr/V-REP/V-REP_PRO_EDU_V3_3_1_64_Linux/scenes/Maze .
-cp -r /home/rodr/V-REP/V-REP_PRO_EDU_V3_3_1_64_Linux/models/corridors .
+
+cp -r /home/rodr/V-REP/V-REP_PRO_EDU_V3_3_1_64_Linux/scenes/Maze . 
+
+cp -r /home/rodr/V-REP/V-REP_PRO_EDU_V3_3_1_64_Linux/models/corridors . 
+
 cp -r /home/rodr/V-REP/V-REP_PRO_EDU_V3_3_1_64_Linux/models/Module .
+
