1ce3c66abd3be3da5f463d761b2143decbb8e5d2
rodr <rodr@rodr-VirtualBox>
2016-09-15 17:07:32 -0500

    Modified HormControlTest to visualize controllers

--- VREPControl/src/Tests/HormControlTest.java
+++ VREPControl/src/Tests/HormControlTest.java
@@ -11,13 +11,13 @@ public class HormControlTest {
 				int simulator= 0;
 				
 				// Morphology Parameters
-						int Numberofmodules = 3;
-						int[] orientation = new int[] { 1, 0, 1};
+						int Numberofmodules = 8;
+						int[] orientation = new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
 						
 						// Simulation Parameters
 						int MaxTime = 30;
 						
-						float[] ampset = new float []{0.5f,0.4f,0.3f,0.5f,0.6f,0.3f,0.5f};
+						/*float[] ampset = new float []{0.5f,0.4f,0.3f,0.5f,0.6f,0.3f,0.5f};
 						float[] offsetset = new float[] {0.1f,-0.1f,0.2f,0.3f,-0.4f,0.3f,0.4f} ;
 						float[] phaseset = new float[] {0.3f,0.2f,0.35f,0.4f,0.3f,0.2f,0.3f};
 						float[] ampstep = new float []{0.01f,0.01f,0.01f,0.01f,0.01f,0.01f,0.01f};
@@ -32,6 +32,13 @@ public class HormControlTest {
 							CP[i+21] = ampstep[i];
 							CP[i+28] = offstep[i];
 							CP[i+35] = phasestep[i];
+						}*/
+						
+		double[] values = new double[] {-0.5298734718668228, 1.0, -1.0, -1.0, 1.0, -0.39413958000362403, 0.3650443550379747, -1.0, 1.0, 0.7125335980963801, 1.0, -0.008876361337063043, -0.5770563431447173, 0.5724250667395164, -1.0, -1.0, -0.987802348345221, -0.8220043327253795, -1.0, -0.8, -0.09516716462731512, 0.4790909282571704, -1.0, -0.06664932800000001, 1.0, -1.0, -1.0, -0.22379606147396336, 1.0, -0.5355456811277005, -0.42433729302519657, 0.6436565809384294, -0.02640908399800146, 0.9999137617018253, -1.0, 1.0, 1.0, -0.3469889860060943, -0.6175689996196543, 1.0, -1.0, -0.7780120651200579};
+						// Control Parameters
+						float[] CP = new float[42];
+						for (int i = 0; i < values.length; i++) {
+							CP[i] = (float) values[i];
 						}
 						
 						// Array that receives fitness from the simulator or signals a crash
@@ -43,7 +50,8 @@ public class HormControlTest {
 							{ 'b', 's', 'l', 's', 's', 'r', 's' },
 							{ 'b', 's', 'r', 's', 's', 'l', 's' },
 							{ 's', 'r', 's', 's', 'l', 's', 'b' },
-							{ 's', 'r', 's', 'b', 's', 'l', 's' } };
+							{ 's', 'r', 's', 'b', 's', 'l', 's' },
+							{'s','s'}};
 						
 						Robot robot = new Robot(Numberofmodules,orientation,CP);
 						
@@ -52,7 +60,7 @@ public class HormControlTest {
 						if(sim.Connect()){
 							
 							sim.SendSignals();
-							sim.SendMaze(subenvperm[0]);
+							sim.SendMaze(subenvperm[6],0.8f);
 							
 							rfitness = sim.RunSimulation(0.7f);
 							
