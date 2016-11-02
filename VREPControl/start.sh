5f3826831e363d02012f102705a79c797e6105c3
rodr <rodr@rodr-VirtualBox>
2016-11-01 19:23:36 -0500

    Added signal acknowledge before simulation start to MDebug.ttt
    Adjusted start.sh to start java program

--- VREPControl/start.sh
+++ VREPControl/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for i in {0,1}
+for i in {0..7}
 do
-    stdbuf -oL /home/rodr/V-REP/Vrep$i/vrep$i.sh -h scenes/Maze/MDebug.ttt >>/home/rodr/git/Evolution/VREPControl/Simout/log$i 2>&1 &	
+    stdbuf -oL /home/rodr/V-REP/Vrep$i/vrep$i.sh -h scenes/Maze/MDebug.ttt >>./Simout/log$i 2>&1 &	
 done
 echo "Finished starting simulators"
+
+sleep 20s
+
+echo "Starting java program"
+
+java -Djava.library.path=/home/rodr/Evolution/ -jar HAEAP.jar 8
+
+echo "Finished"
