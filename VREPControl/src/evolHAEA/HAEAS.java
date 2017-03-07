d33add7cf5ec794381ac1516c24095304f1cd526
rodr <rodr@rodr-VirtualBox>
2017-03-07 12:06:59 -0500

    Created single threaded evaluation HAEA evolution class

---
+++ VREPControl/src/evolHAEA/HAEAS.java
@@ -0,0 +1,109 @@
+package evolHAEA;
+
+import java.io.File;
+import java.lang.ProcessBuilder.Redirect;
+
+import simvrep.Simulation;
+import unalcol.descriptors.Descriptors;
+import unalcol.descriptors.WriteDescriptors;
+import unalcol.evolution.haea.HAEA;
+import unalcol.evolution.haea.HaeaOperators;
+import unalcol.evolution.haea.SimpleHaeaOperators;
+import unalcol.io.Write;
+import unalcol.optimization.OptimizationFunction;
+import unalcol.optimization.OptimizationGoal;
+import unalcol.optimization.real.HyperCube;
+import unalcol.optimization.real.mutation.AdaptMutationIntensity;
+import unalcol.optimization.real.mutation.GaussianMutation;
+import unalcol.optimization.real.mutation.IntensityMutation;
+import unalcol.optimization.real.mutation.OneFifthRule;
+import unalcol.optimization.real.xover.LinearXOver;
+import unalcol.search.Goal;
+import unalcol.search.Solution;
+import unalcol.search.SolutionDescriptors;
+import unalcol.search.population.variation.ArityTwo;
+import unalcol.search.population.variation.Operator;
+import unalcol.search.selection.Tournament;
+import unalcol.search.space.Space;
+import unalcol.tracer.ConsoleTracer;
+import unalcol.tracer.FileTracer;
+import unalcol.tracer.Tracer;
+import unalcol.types.real.array.DoubleArray;
+import unalcol.types.real.array.DoubleArrayPlainWrite;;
+
+public class HAEAS {
+
+	public static Simulation sim;
+
+	public static void main(String[] args) {
+
+		sim = new Simulation(0, 30);
+
+		if (!sim.Connect()) {
+			System.exit(-1);
+		}
+
+		// Search Space Definition
+		int DIM = 42;
+		double[] min = DoubleArray.create(DIM, -1);
+		double[] max = DoubleArray.create(DIM, 1);
+
+		Space<double[]> space = new HyperCube(min, max);
+
+		// Optimization function
+		OptimizationFunction<double[]> function = new HDebugP(0.7f, sim);
+		Goal<double[]> goal = new OptimizationGoal<double[]>(function);
+
+		// Variation Definition
+		AdaptMutationIntensity adapt = new OneFifthRule(20, 0.9);
+		IntensityMutation variation = new GaussianMutation(0.1, null, adapt);
+		ArityTwo<double[]> xover = new LinearXOver();
+
+		int POPSIZE = 2;
+		int MAXITERS = 2;
+		Operator<double[]>[] opers = (Operator<double[]>[]) new Operator[2];
+		opers[0] = variation;
+		opers[1] = xover;
+
+		HaeaOperators<double[]> operators = new SimpleHaeaOperators<double[]>(opers);
+		HAEA<double[]> search = new HAEA<double[]>(POPSIZE, operators, new Tournament<double[]>(4), MAXITERS);
+
+		// Track Individuals and Goal Evaluations
+		SolutionDescriptors<double[]> desc = new SolutionDescriptors<double[]>();
+		Descriptors.set(Space.class, desc);
+		DoubleArrayPlainWrite write = new DoubleArrayPlainWrite(',');
+		Write.set(double[].class, write);
+		WriteDescriptors w_desc = new WriteDescriptors();
+		Write.set(Space.class, w_desc);
+
+		// Add tracer based on descriptors set
+		FileTracer tracer = new FileTracer("Evolresult.txt", ',');
+		ConsoleTracer tracer1 = new ConsoleTracer();
+		Tracer.addTracer(search, tracer1);
+		Tracer.addTracer(search, tracer);
+
+		Solution<double[]> solution = search.apply(space, goal);
+
+		System.out.println(solution.quality());
+
+		tracer.close();
+		tracer1.close();
+
+		sim.Disconnect();
+
+		// kill all the v-rep processes
+		try {
+			ProcessBuilder qq = new ProcessBuilder("killall", "vrep" + 0);
+			File log = new File("Simout/log");
+			qq.redirectErrorStream(true);
+			qq.redirectOutput(Redirect.appendTo(log));
+			Process p = qq.start();
+			int exitVal = p.waitFor();
+			System.out.println("Terminated vrep" + 0 + " with error code " + exitVal);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+		}
+
+	}
+}
--- VREPControl/src/evolHAEA/HDebugP.java
+++ VREPControl/src/evolHAEA/HDebugP.java
@@ -7,146 +7,164 @@ import simvrep.Simulation;
 import unalcol.optimization.OptimizationFunction;
 import unalcol.types.collection.bitarray.BitArray;
 
-public class HDebugP extends OptimizationFunction<double[]>{
-	
+public class HDebugP extends OptimizationFunction<double[]> {
+
 	boolean DEBUG = false;
-	
+
 	protected List<Simulation> simulators;
-	
+
+	protected Simulation sim;
+
 	protected BitArray servers;
 	public float alpha = 0.7f;
-	
+	protected boolean paralleleval = false;
+
 	public HDebugP(int numberOfServers, List<Simulation> simulators) {
 		servers = new BitArray(numberOfServers, false);
 		this.simulators = simulators;
-		if (DEBUG){
+		if (DEBUG) {
 			System.out.println("Building HDebugP");
 		}
+		paralleleval = true;
 	}
-	
+
+	public HDebugP(int numberOfServers, float alpha) {
+		if (DEBUG) {
+			System.out.println("Building HDebugP");
+		}
+		servers = new BitArray(numberOfServers, false);
+		this.alpha = alpha;
+		paralleleval = true;
+	}
+
+	public HDebugP(float alpha, Simulation sim) {
+		this.alpha = alpha;
+		this.sim = sim;
+		paralleleval = false;
+	}
+
 	public synchronized int getSimNumber() {
-		if (DEBUG){
+		if (DEBUG) {
 			System.out.println("Using getSimNumber()");
 		}
-		for(int i = 0;i < servers.size(); ++i)
-			if(!servers.get(i)) {
+		for (int i = 0; i < servers.size(); ++i)
+			if (!servers.get(i)) {
 				servers.set(i, true);
 				return i;
 			}
 		return -1;
 	}
-	
-	HDebugP(int numberOfServers, float alpha) {
-		if (DEBUG){
-			System.out.println("Building HDebugP");
-		}
-		servers = new BitArray(numberOfServers, false);
-		this.alpha = alpha;
-	}
-	
-	public synchronized int waitforsim(){
-		if (DEBUG){
+
+	public synchronized int waitforsim() {
+		if (DEBUG) {
 			System.out.println("Using waitforsim()");
 		}
 		int sim = -1;
-		while(sim < 0) sim = getSimNumber();
+		while (sim < 0)
+			sim = getSimNumber();
 		return sim;
-		
+
 	}
-	
+
 	public Double apply(double[] x) {
-		if (DEBUG){
+		if (DEBUG) {
 			System.out.println("Using apply()");
 		}
-		int simulator = -1;
-		simulator = waitforsim();
-		
-		System.out.println("Got sim: "+simulator);
-		
+
+		int simulator = 0;
+
+		if (paralleleval) {
+			simulator = -1;
+			simulator = waitforsim();
+			System.out.println("Got sim: " + simulator);
+		}
+
 		// Morphology Parameters
-				int Numberofmodules = 8;
-				int[] orientation = new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
-				
-				// Simulation Parameters
-				//int MaxTime = 30;
+		int Numberofmodules = 8;
+		int[] orientation = new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
 
-				// Control Parameters
-				float[] CP = new float[42];
-				
-				//CPG set parameters for each hormone
-				for (int i = 0; i < 21; i++) {
-					CP[i] = (float) x[i];
+		// Simulation Parameters
+		// int MaxTime = 30;
+
+		// Control Parameters
+		float[] CP = new float[42];
+
+		// CPG set parameters for each hormone
+		for (int i = 0; i < 21; i++) {
+			CP[i] = (float) x[i];
+		}
+		// CPG step parameters for each hormone
+		for (int i = 21; i < 42; i++) {
+			CP[i] = (float) x[i] * 0.01f;
+		}
+
+		// Array that receives fitness from the simulator or signals a crash
+		float[] rfitness = new float[3];
+
+		char[][] subenvperm = new char[][] { { 's', 'l', 's', 'b', 's', 'r', 's' },
+				{ 's', 'l', 's', 's', 'r', 's', 'b' }, { 'b', 's', 'l', 's', 's', 'r', 's' },
+				{ 'b', 's', 'r', 's', 's', 'l', 's' }, { 's', 'r', 's', 's', 'l', 's', 'b' },
+				{ 's', 'r', 's', 'b', 's', 'l', 's' }, { 's', 's' } };
+
+		Robot robot = new Robot(Numberofmodules, orientation, CP);
+
+		if (paralleleval) {
+			sim = simulators.get(simulator);
+		}
+
+		// Number of retries in case of simulator crash
+		int maxTries = 5;
+
+		// Retry if there is a simulator crash
+		for (int j = 0; j < maxTries; j++) {
+
+			// if (sim.Connect()) {
+			sim.prepareSignals(robot);
+			sim.SendSignals();
+			sim.SendMaze(subenvperm[6], 0.8f);
+
+			try {
+				rfitness = sim.RunSimulation(alpha);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// sim.Disconnect();
+
+			if (rfitness[0] == -1) {
+				// sim.RestartSim(j, "MRun.ttt");
+				// continue;
+				long startTime = System.currentTimeMillis();
+				long stopTime = System.currentTimeMillis();
+				long elapsedTime = 0;
+
+				elapsedTime = stopTime - startTime;
+
+				while (elapsedTime < 60000) {
+					stopTime = System.currentTimeMillis();
+					elapsedTime = stopTime - startTime;
 				}
-				//CPG step parameters for each hormone
-				for (int i = 21; i < 42;i++){
-					CP[i] = (float) x[i]*0.01f;
-				}
-				
-				// Array that receives fitness from the simulator or signals a crash
-				float[] rfitness = new float[3];
 
-				char[][] subenvperm = new char[][] {
-					{ 's', 'l', 's', 'b', 's', 'r', 's' },
-					{ 's', 'l', 's', 's', 'r', 's', 'b' },
-					{ 'b', 's', 'l', 's', 's', 'r', 's' },
-					{ 'b', 's', 'r', 's', 's', 'l', 's' },
-					{ 's', 'r', 's', 's', 'l', 's', 'b' },
-					{ 's', 'r', 's', 'b', 's', 'l', 's' },
-					{'s','s'}};
+				System.exit(0);
 
-				Robot robot = new Robot(Numberofmodules, orientation, CP);
+			}
 
-				Simulation sim = simulators.get(simulator);
-				
-				// Number of retries in case of simulator crash
-				int maxTries = 5;
-				
-				// Retry if there is a simulator crash
-				for (int j = 0; j < maxTries; j++) {
+			// } else {
+			// No connection could be established
+			// System.out.println("Failed connecting to remote API server");
+			// System.out.println("Trying again for the " + j + " time in " +
+			// simulator);
+			// continue;
+			// }
+			break;
+		}
 
-					//if (sim.Connect()) {
-						sim.prepareSignals(robot);
-						sim.SendSignals();
-						sim.SendMaze(subenvperm[6], 0.8f);
+		if (paralleleval) {
+			servers.set(simulator, false);
+		}
 
-						try {
-							rfitness = sim.RunSimulation(alpha);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-
-						//sim.Disconnect();
-
-						if (rfitness[0] == -1) {
-							//sim.RestartSim(j, "MRun.ttt");
-							//continue;
-							long startTime = System.currentTimeMillis();
-							long stopTime = System.currentTimeMillis();
-							long elapsedTime = 0;
-							
-							elapsedTime = stopTime-startTime;
-							
-							while (elapsedTime < 60000){
-								stopTime = System.currentTimeMillis();
-								elapsedTime = stopTime-startTime;
-							}
-							
-							System.exit(0);
-							
-						}
-
-					//} else {
-						// No connection could be established
-					//	System.out.println("Failed connecting to remote API server");
-					//	System.out.println("Trying again for the " + j + " time in " + simulator);
-					//	continue;
-					//}
-					break;
-				}
-				
-				servers.set(simulator, false);
-				return (double) rfitness[1];
+		return (double) rfitness[1];
 	}
 
 }
