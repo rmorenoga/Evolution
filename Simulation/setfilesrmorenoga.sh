ca10e281b5fd217859dbd71db37eba3f71766940
rmorenoga <rmorenoga@MIDAS>
2017-05-17 17:04:05 -0500

    Included script for copying simulation files in rmorenoga

---
+++ Simulation/setfilesrmorenoga.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copies the simulation files in the corresponding simulators folders
+#
+
+
+
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep0/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep1/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep2/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep3/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep4/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep5/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep6/scenes/
+cp -r ./Maze /home/rmorenoga/V-REP/Vrep7/scenes/
+
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep0/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep1/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep2/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep3/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep4/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep5/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep6/models/
+cp -r ./corridors /home/rmorenoga/V-REP/Vrep7/models/
+
+cp -r ./Module /home/rmorenoga/V-REP/Vrep0/models/ 
+cp -r ./Module /home/rmorenoga/V-REP/Vrep1/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep2/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep3/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep4/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep5/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep6/models/
+cp -r ./Module /home/rmorenoga/V-REP/Vrep7/models/
+
+cp -r ./modular /home/rmorenoga/V-REP/Vrep0/lua/ 
+cp -r ./modular /home/rmorenoga/V-REP/Vrep1/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep2/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep3/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep4/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep5/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep6/lua/
+cp -r ./modular /home/rmorenoga/V-REP/Vrep7/lua/
+
+
+
