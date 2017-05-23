a03696b9d20e5db83a93072e5eaca50009a5f7a2
golde_000 <golde_000@rodrigo-pc>
2017-05-22 19:42:38 -0500

    Commented chromo creation process in Em module ShowHFitReal.
    Now we can design robots manually by modifying the chromo code directly

--- VREPControl/src/show/ShowHFitReal.java
+++ VREPControl/src/show/ShowHFitReal.java
@@ -9,12 +9,17 @@ public class ShowHFitReal {
 
 	public static void main(String[] args) {
 		
-		Simulation sim = new Simulation(0, 30);
+		Simulation sim = new Simulation(0, 10);
 		double fitness =1000;
-		String morpho = "[(0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 3.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 3.0, 2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]";
-		
+		String morpho = "[(0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0 , 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]";
+		// [( 'Type' 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'Conn' 1.0, 3.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'DadFace' 0.0, 3.0, 2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 'Ori'0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+		// Type: Only two types available 0 and 1, the root is always 0 and the rest are always 1
+		// Conn: The number of modules connected to that module, the first value are modules connected to the root module and so on
+		// DadFace: The face to which the module is connected in the parent, root module only has 0,1,2 faces the rest have 1,2,3 since 0 is always occupied by the parent
+		// Ori: The orientation of the module, only two orientations available 0 and 1
+		// Each position is evaluated after the other: For each module with a type, the number of modules is retrieved, then the children are retrieved and connected to the specified dadfaces, this continues until no more children are found
 		//float[] parameters = new float[212];
-		double[] parameters = new double[212];
+		double[] parameters = new double[282];
 		
 //		char[][] subenvperm = new char[][] { { 's', 'l', 's', 'b', 's', 'r', 's' },
 //			{ 's', 'l', 's', 's', 'r', 's', 'b' }, { 'b', 's', 'l', 's', 's', 'r', 's' },
@@ -24,7 +29,7 @@ public class ShowHFitReal {
 //		float width = 0.7f;
 		
 		for (int i = 0;i<parameters.length;i++){
-			parameters[i]=0.5;
+			parameters[i]=0;
 		}
 		
 		for (int i = 0; i < 5; i++) {
