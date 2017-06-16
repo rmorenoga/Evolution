92e2eda227f47259823171fd4cb3975e2900570b
rmorenoga <rmorenoga@MIDAS>
2017-06-15 19:12:22 -0500

    Fixed communication problem in CPG Emerge modules.
    Fixed bug that didn't allow RobotController adjustParameter method to
    handle CPG controllers

--- VREPControl/src/control/RobotController.java
+++ VREPControl/src/control/RobotController.java
@@ -10,7 +10,7 @@ import simvrep.RobotBuilder;
 import simvrep.SimulationConfiguration;
 
 public class RobotController {
-	
+
 	/**
 	 * Signals to set in the simulator
 	 */
@@ -18,9 +18,8 @@ public class RobotController {
 	public CharWA strEP;
 	public CharWA strMH;
 	public CharWA strConn;
-	
+
 	int[] connectedhandles;
-	
 
 	protected String controllerName;
 	protected remoteApi vrep;
@@ -32,13 +31,13 @@ public class RobotController {
 	float[] parameters;
 	float[] extraparam;
 	int extrap = -1;
-	
+
 	public RobotController(remoteApi vrep, int clientID, RobotBuilder robot, float[] parameters, float[] extraparam) {
 		this(vrep, clientID, robot, parameters);
 		this.extraparam = extraparam;
 		this.extrap = extraparam.length;
 		adjustextraParam();
-		//System.out.println("extrap "+extrap);
+		// System.out.println("extrap "+extrap);
 	}
 
 	public RobotController(remoteApi vrep, int clientID, RobotBuilder robot, float[] parameters) {
@@ -47,100 +46,130 @@ public class RobotController {
 		this.robot = robot;
 		moduleHandlers = robot.getModuleHandlersint();
 		this.numberofModules = moduleHandlers.length;
-		this.numberofParameters = SimulationConfiguration.getControllerparamnumber();	
+		this.numberofParameters = SimulationConfiguration.getControllerparamnumber();
 		connectedhandles = robot.getTree().getHandlerListint();
-		if (parameters.length >= numberofParameters*numberofModules){
+		if (parameters.length >= numberofParameters * numberofModules) {
 			this.parameters = parameters;
 		} else {
 			System.err.println("CPGController");
 			System.err.println("Error in the number of parameters, parameters lenght=" + parameters.length);
 			System.exit(-1);
 		}
-			adjustParam();
+		adjustParam(SimulationConfiguration.getController());
 	}
-	
-	private void adjustextraParam(){
+
+	private void adjustextraParam() {
 		float[] grownextra = new float[extraparam.length];
-		for (int i = 0; i <extraparam.length;i++){
-			grownextra[i] = ((extraparam[i] + 1)/2); 
+		for (int i = 0; i < extraparam.length; i++) {
+			grownextra[i] = ((extraparam[i] + 1) / 2);
 		}
 		extraparam = grownextra;
 	}
-	
-	private void adjustParam(){
-		float maxPhase = (float) SimulationConfiguration.getMaxPhase();
-		float minPhase = (float) SimulationConfiguration.getMinPhase();
-		float maxAmplitude = (float) SimulationConfiguration.getMaxAmplitude();
-		float minAmplitude  = (float) SimulationConfiguration.getMinAmplitude();
-		float maxOffset = (float) SimulationConfiguration.getMaxOffset();
-		float minOffset = (float) SimulationConfiguration.getMinOffset();
-		float maxFreq = (float) SimulationConfiguration.getMaxAngularFreq();
-		float minFreq = (float) SimulationConfiguration.getMinAngularFreq();
-		
-		//Assuming min raw parameter is -1 and max is 1
-		//NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
-		//NewValue = (((OldValue - (-1)) * (NewMax - NewMin)) / (1 - (-1))) + NewMin
+
+	private void adjustParam(String controltype) {
 		float[] grownparam = new float[parameters.length];
-		for (int i = 0; i<parameters.length; i = i + numberofParameters){
-			for (int j = 0;j<5; j++){
-				grownparam[i+j] = (((parameters[i+j] + 1)*(maxAmplitude-minAmplitude))/2)+minAmplitude;
-				grownparam[i+j+5] = (((parameters[i+j+5] + 1)*(maxOffset-minOffset))/2)+minOffset;
-				grownparam[i+j+30] = (((parameters[i+j+30] + 1)*(maxFreq-minFreq))/2)+minFreq;
+		if (controltype.contentEquals("CPG")) {
+			float maxPhase = (float) SimulationConfiguration.getMaxPhase();
+			float minPhase = (float) SimulationConfiguration.getMinPhase();
+			float maxAmplitude = (float) SimulationConfiguration.getMaxAmplitude();
+			float minAmplitude = (float) SimulationConfiguration.getMinAmplitude();
+			float maxOffset = (float) SimulationConfiguration.getMaxOffset();
+			float minOffset = (float) SimulationConfiguration.getMinOffset();
+			// Assuming min raw parameter is -1 and max is 1
+						// NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax -
+						// OldMin)) + NewMin
+						// NewValue = (((OldValue - (-1)) * (NewMax - NewMin)) / (1 - (-1)))
+						// + NewMin
+			for (int i = 0; i < parameters.length; i = i + numberofParameters) {	
+					grownparam[i] = (((parameters[i] + 1) * (maxAmplitude - minAmplitude)) / 2) + minAmplitude;
+					grownparam[i+1] = (((parameters[i+1] + 1) * (maxOffset - minOffset)) / 2) + minOffset;
+				for (int j = 0; j < 4; j++) {
+					grownparam[i + j + 2] = (((parameters[i + j + 2] + 1) * (maxPhase - minPhase)) / 2) + minPhase;
+				}
 			}
-			for (int j = 0;j<20; j++){
-				grownparam[i+j+10] = (((parameters[i+j+10] + 1)*(maxPhase-minPhase))/2)+minPhase;
+			
+		} else if(controltype.contentEquals("CPGH")||controltype.contentEquals("CPGHF")) {
+			float maxPhase = (float) SimulationConfiguration.getMaxPhase();
+			float minPhase = (float) SimulationConfiguration.getMinPhase();
+			float maxAmplitude = (float) SimulationConfiguration.getMaxAmplitude();
+			float minAmplitude = (float) SimulationConfiguration.getMinAmplitude();
+			float maxOffset = (float) SimulationConfiguration.getMaxOffset();
+			float minOffset = (float) SimulationConfiguration.getMinOffset();
+			float maxFreq = (float) SimulationConfiguration.getMaxAngularFreq();
+			float minFreq = (float) SimulationConfiguration.getMinAngularFreq();
+
+			// Assuming min raw parameter is -1 and max is 1
+			// NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax -
+			// OldMin)) + NewMin
+			// NewValue = (((OldValue - (-1)) * (NewMax - NewMin)) / (1 - (-1)))
+			// + NewMin
+
+			for (int i = 0; i < parameters.length; i = i + numberofParameters) {
+				for (int j = 0; j < 5; j++) {
+					grownparam[i + j] = (((parameters[i + j] + 1) * (maxAmplitude - minAmplitude)) / 2) + minAmplitude;
+					grownparam[i + j + 5] = (((parameters[i + j + 5] + 1) * (maxOffset - minOffset)) / 2) + minOffset;
+					grownparam[i + j + 30] = (((parameters[i + j + 30] + 1) * (maxFreq - minFreq)) / 2) + minFreq;
+				}
+				for (int j = 0; j < 20; j++) {
+					grownparam[i + j + 10] = (((parameters[i + j + 10] + 1) * (maxPhase - minPhase)) / 2) + minPhase;
+				}
 			}
 		}
 		parameters = grownparam;
 	}
-	
-public void sendParameters() {
-		
-		for (int i = 0; i < moduleHandlers.length ;i++){
+
+	public void sendParameters() {
+
+		for (int i = 0; i < moduleHandlers.length; i++) {
 			moduleHandlers[i] = moduleHandlers[i] + 1;
 		}
 		
+		for (int i = 0; i < connectedhandles.length; i++) {
+			if(connectedhandles[i] != -1){
+			connectedhandles[i] = connectedhandles[i] + 1;
+			}
+		}
+
 		FloatWA ControlParam = new FloatWA(parameters.length);
-		System.arraycopy(parameters,0,ControlParam.getArray(),0,parameters.length);
+		System.arraycopy(parameters, 0, ControlParam.getArray(), 0, parameters.length);
 		char[] p = ControlParam.getCharArrayFromArray();
 		strCP = new CharWA(p.length);
-		System.arraycopy(p,0,strCP.getArray(),0,p.length);
-		
+		System.arraycopy(p, 0, strCP.getArray(), 0, p.length);
+
 		IntWA Connhandles = new IntWA(connectedhandles.length);
-		System.arraycopy(connectedhandles,0,Connhandles.getArray(),0,connectedhandles.length);
+		System.arraycopy(connectedhandles, 0, Connhandles.getArray(), 0, connectedhandles.length);
 		char[] q = Connhandles.getCharArrayFromArray();
 		strConn = new CharWA(q.length);
-		System.arraycopy(q,0,strConn.getArray(),0,q.length);
-		
+		System.arraycopy(q, 0, strConn.getArray(), 0, q.length);
+
 		IntWA Modhandles = new IntWA(moduleHandlers.length);
-		System.arraycopy(moduleHandlers,0,Modhandles.getArray(),0,moduleHandlers.length);
+		System.arraycopy(moduleHandlers, 0, Modhandles.getArray(), 0, moduleHandlers.length);
 		char[] r = Modhandles.getCharArrayFromArray();
 		strMH = new CharWA(r.length);
-		System.arraycopy(r,0,strMH.getArray(),0,r.length);
-		
-		if(extrap>0){
+		System.arraycopy(r, 0, strMH.getArray(), 0, r.length);
+
+		if (extrap > 0) {
 			FloatWA ExtraParam = new FloatWA(extraparam.length);
-			System.arraycopy(extraparam,0,ExtraParam.getArray(),0,extraparam.length);
+			System.arraycopy(extraparam, 0, ExtraParam.getArray(), 0, extraparam.length);
 			char[] ep = ExtraParam.getCharArrayFromArray();
 			strEP = new CharWA(ep.length);
-			System.arraycopy(ep,0,strEP.getArray(),0,ep.length);
+			System.arraycopy(ep, 0, strEP.getArray(), 0, ep.length);
 		}
-		
-		//Pause communication
+
+		// Pause communication
 		vrep.simxPauseCommunication(clientID, true);
 		// Set Simulator signal values
 		int result1 = vrep.simxSetStringSignal(clientID, "ControlParam", strCP, vrep.simx_opmode_oneshot);
 		int result2 = vrep.simxSetStringSignal(clientID, "ConnHandles", strConn, vrep.simx_opmode_oneshot);
 		int result3 = vrep.simxSetStringSignal(clientID, "ModHandles", strMH, vrep.simx_opmode_oneshot);
-		if(extrap>0){
+		if (extrap > 0) {
 			int result4 = vrep.simxSetStringSignal(clientID, "ExtraParam", strEP, vrep.simx_opmode_oneshot);
 		}
-		//Unpause communication
-		vrep.simxPauseCommunication(clientID,false);
-		
-		
+		// Unpause communication
+		vrep.simxPauseCommunication(clientID, false);
+
 	}
-	
+
 	public String getControllerName() {
 		return controllerName;
 	}
@@ -157,13 +186,9 @@ public void sendParameters() {
 		return parameters.length;
 	}
 
-
-
 	public float[] getParameters() {
 		return parameters;
 	}
-
-
 
 	public void setParameters(float[] parameters) {
 		this.parameters = parameters;
